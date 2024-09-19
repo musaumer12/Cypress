a0688adea9745c141b96a65218090386ce2a7337
Dev <dev@Devs-MacBook-Pro-101.local>
2024-09-19 17:18:36 +0500

    Drop down and wait changes in the Testing file

--- cypress/e2e/testing.cy.js
+++ cypress/e2e/testing.cy.js
@@ -1,49 +1,58 @@
 it('Assignment', () => {
+    cy.visit('https://iodinesoftware.com/');
     
+    // Open the menu and navigate to the Careers page
+    cy.get('.hamburger-box').click();
+    cy.get('.drop-down-arrow').eq(5).click();
+    cy.get('#menu-item-4069 > a').invoke('removeAttr', 'target').click();
     
-    cy.visit('https://iodinesoftware.com/')
-    
-    cy.get('.hamburger-box').click()
-    cy.get('.drop-down-arrow').eq(5).click()
-    cy.get('#menu-item-4069 > a').invoke('removeAttr', 'target').click();  // used a plugin for Tab switching and Put it in comnmand.js file
-    
-    // cy.visit('https://iodinesoftware.com/careers-2/') // Can also be done by directly hitting the link continue the actions
+    // Alias the iframe for reuse
+    cy.get('#gnewtonIframe').as('iframe');
 
     // Access and interact with the iframe
-    cy.get('#gnewtonIframe')
-        .iframe() // Custom command to get iframe body (Stored in command.js)
+    cy.get('@iframe')
+        .iframe() // Custom command to get iframe body
         .find('a[href="https://recruitingbypaycor.com/career/JobIntroduction.action?clientId=8a7883d077926ee10177975a28d104f6&id=8a7883a88f5ae663018fa1af2a4c1428&source=&lang=en"]')
         .should('be.visible')
-
-        .click() 
-    
-    cy.wait(2000); 
-
-    // Interact with the updated content
-    cy.get('#gnewtonIframe')
-        .iframe() // Custom command to get iframe body same as above (same method to be used to get all elements inside the iframe body)
-        .contains('Apply for this Position') 
-        .should('be.visible') 
         .click();
-
-    cy.wait(2000);
-    cy.get('#gnewtonIframe')
+    // Break the chain to handle potential re-rendering
+    cy.wait(2000)
+    cy.get('@iframe')
+        .iframe()
+        .contains('Apply for this Position')
+        .should('exist')   // Check if element exists
+        .and('be.visible') // Ensure it is visible
+        .click();
+    cy.wait(2000)
+    // Fill in the form inside the iframe
+    cy.get('@iframe')
         .iframe()
         .find('input.gnewtonRequired')
         .eq(1)
         .should('be.visible')
         .click()
-        .wait(2000) // added wait because the frame was not laoding and text was not typed in the field
-        .type("Musa Umer", {force: true})
+        .type("Musa Umer", { force: true });
 
-    cy.get('#gnewtonIframe')
+    // Ensure last name field is visible and empty before interacting
+    cy.get('@iframe')
         .iframe()
         .find('#lastName')
         .should('be.visible')
-        .should('be.empty')
-    cy.get('#gnewtonIframe')
+        .should('be.empty');
+
+    // Upload a file
+    cy.get('@iframe')
+        .iframe()
+        .find('#resumeDropLocalFile')
+        .click()
+        .selectFile('/Users/dev/Downloads/username.csv');
+
+    cy.get('@iframe')
     .iframe()
-    .find('#resumeDropLocalFile')
-    .click()
-    .selectFile('/Users/dev/Downloads/username.csv') //Uploading file
+    .find('#gnewton_section_80_question_10_answer')
+    .select('Yes')
+    
+
+
+
     });
